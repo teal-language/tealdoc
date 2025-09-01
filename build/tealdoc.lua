30802d1703f010bc7b5c50d2c55bcf777e23e8c2
uped <mikikocz@gmail.com>
2025-09-01 13:28:50 +0200

    update version string

--- build/tealdoc.lua
+++ build/tealdoc.lua
@@ -299,7 +299,7 @@ local tealdoc = { Location = {}, Env = {}, Typearg = {}, FunctionItem = { Param 
 
 
 
-tealdoc.version = "0.3"
+tealdoc.version = "0.3+dev"
 
 function tealdoc.Env.init()
    local env = {
